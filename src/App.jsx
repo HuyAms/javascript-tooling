266e88e4e682f36a7a102b20d2353d5038894270
huytrinh <dinhhuyams@gmail.com>
2021-06-17 23:15:33 +0300

    chore: add project starter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+    return (
+        <>
+            <h1>JavaScript tooling</h1>
+            <p>Welcome to the presentation</p>
+        </>
+    )
+}
+
+export default App
