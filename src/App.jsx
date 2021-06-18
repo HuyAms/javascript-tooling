54df8f0c4521ee261fd9bc0410943e14c69c3c79
huytrinh <dinhhuyams@gmail.com>
2021-06-18 11:08:40 +0300

    broken code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 
 function App() {
+
+    const unsedVariable = 5
+
     return (
         <>
-            <h1>JavaScript tooling</h1>
-            <p>Welcome to the presentation</p>
+                             <h1>JavaScript tooling</h1>
+
+
+                         <p>Welcome to the presentation</p>
         </>
     )
 }
